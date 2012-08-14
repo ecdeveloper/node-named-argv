4f97f72ba72731f9afdc526252eea7330277070e
patison <patison@patison-desktop.(none)>
2012-08-14 17:40:52 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var argOptions = {},
+	argParams  = [];
+
+var rx = /^-?-(\w+)(?:=(.*))?$/;
+var argv = process.argv;
+
+argv.shift();
+argv.shift();
+
+argv.forEach(function(item) {
+	if (matches = item.match(rx)) {
+		var key = matches[1];
+		var val = matches[2] ? matches[2] : true;
+
+		if (argOptions[key]!=undefined) {
+			if( !(typeof argOptions[key] == "Array") )
+				argOptions[key] = [argOptions[key]];
+
+			argOptions[key].push(val);
+		}
+		else
+			argOptions[key] = val;
+	}
+	else
+		argParams.push(item);
+});
+
+exports.opts = argOptions;
+exports.params = argParams;
